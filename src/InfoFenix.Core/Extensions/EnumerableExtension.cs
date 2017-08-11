97193afabf44ae19899689a0644d0d17fc6e55a1
marcot <marco.teixeira@me.com.br>
2017-08-11 17:40:41 -0300

    Removed unused classes

--- src/InfoFenix.Core/Extensions/EnumerableExtension.cs
+++ src/InfoFenix.Core/Extensions/EnumerableExtension.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 
 namespace InfoFenix.Core {
+
     /// <summary>
     /// Extension methods for <see cref="IEnumerable"/> and <see cref="IEnumerable{T}"/>.
     /// </summary>
@@ -115,73 +112,6 @@ namespace InfoFenix.Core {
             return !canMoveNext;
         }
 
-        /// <summary>
-        /// Converts an <see cref="IEnumerable{T}"/> instance into a <see cref="IReadOnlyCollection{T}"/>.
-        /// </summary>
-        /// <typeparam name="T">The type of the enumerable.</typeparam>
-        /// <param name="source">The source <see cref="IEnumerable{T}"/>.</param>
-        /// <returns>An <see cref="IReadOnlyCollection{T}"/> instance.</returns>
-        public static IList<T> ToReadOnlyCollection<T>(this IEnumerable<T> source) {
-            return new ReadOnlyCollection<T>((source ?? Enumerable.Empty<T>()).ToList());
-        }
-
-        /// <summary>
-        /// Selects distinct the source <see cref="IEnumerable{T}"/> by an expression.
-        /// </summary>
-        /// <typeparam name="TSource">Type of the <see cref="IEnumerable{T}"/></typeparam>
-        /// <typeparam name="TKey">Type of the key.</typeparam>
-        /// <param name="source">The source <see cref="IEnumerable{T}"/>.</param>
-        /// <param name="keySelector">The key selector function.</param>
-        /// <returns>A filtered collection.</returns>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) {
-            var seenKeys = new HashSet<TKey>();
-
-            foreach (var element in source) {
-                if (seenKeys.Add(keySelector(element))) {
-                    yield return element;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Orders an enumerable by its argument type field, specified as <see cref="string"/>.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the enumerable.</typeparam>
-        /// <param name="source">The enumerable.</param>
-        /// <param name="fieldName">The field name in the enumerable type.</param>
-        /// <returns>The ordered result.</returns>
-        public static IEnumerable<TSource> OrderBy<TSource>(this IEnumerable<TSource> source, string fieldName) {
-            return ExecuteOrderBy(source, fieldName, ascending: true);
-        }
-
-        /// <summary>
-        /// Orders, descending, an enumerable by its argument type field, specified as <see cref="string"/>.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the enumerable.</typeparam>
-        /// <param name="source">The enumerable.</param>
-        /// <param name="fieldName">The field name in the enumerable type.</param>
-        /// <returns>The ordered result.</returns>
-        public static IEnumerable<TSource> OrderByDescending<TSource>(this IEnumerable<TSource> source, string fieldName) {
-            return ExecuteOrderBy(source, fieldName, ascending: false);
-        }
-
         #endregion Public Static Methods
-
-        #region Private Static Methods
-
-        private static IEnumerable<TSource> ExecuteOrderBy<TSource>(IEnumerable<TSource> source, string fieldName, bool ascending = true) {
-            var query = source.AsQueryable();
-
-            var type = typeof(TSource);
-            var property = type.GetTypeInfo().GetProperty(fieldName);
-            var parameter = Expression.Parameter(type, "_");
-            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
-            var resultExpression = Expression.Call(typeof(Queryable), ascending ? nameof(Queryable.OrderBy) : nameof(Queryable.OrderByDescending), new Type[] { type, property.PropertyType }, query.Expression, Expression.Quote(orderByExpression));
-
-            return query.Provider.CreateQuery<TSource>(resultExpression);
-        }
-
-        #endregion Private Static Methods
     }
 }
--- src/InfoFenix.Core/Extensions/StringExtension.cs
+++ src/InfoFenix.Core/Extensions/StringExtension.cs
@@ -1,35 +1,10 @@
-﻿using System.Globalization;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 
 namespace InfoFenix.Core {
+
     public static class StringExtension {
 
         #region Public Static Methods
-
-        /// <summary>
-        /// Remove diacritics from <paramref name="source"/> <see cref="string"/>.
-        /// Diacritics are signs, such as an accent or cedilla, which when written above or below a letter indicates
-        /// a difference in pronunciation from the same letter when unmarked or differently marked.
-        /// </summary>
-        /// <param name="source">The source <see cref="string"/>.</param>
-        /// <returns>A new <see cref="string"/> without diacritics.</returns>
-        public static string RemoveDiacritics(this string source) {
-            if (string.IsNullOrWhiteSpace(source)) {
-                return source;
-            }
-
-            var normalized = source.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder();
-
-            foreach (var @char in normalized) {
-                if (CharUnicodeInfo.GetUnicodeCategory(@char) != UnicodeCategory.NonSpacingMark) {
-                    stringBuilder.Append(@char);
-                }
-            }
-
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
 
         public static string RemoveHtmlTags(this string source, bool htmlDecode = false) {
             if (string.IsNullOrEmpty(source)) {
@@ -63,12 +38,6 @@ namespace InfoFenix.Core {
             }
 
             return result;
-        }
-
-        public static byte[] ToByteArray(this string source) {
-            if (source == null) { return null; }
-
-            return Encoding.UTF8.GetBytes(source);
         }
 
         #endregion Public Static Methods
